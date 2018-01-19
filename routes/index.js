1406e1596a98193e818cd87025617f94ecca751f
chulhun-hwang <qewqsa@naver.com>
2018-01-19 17:53:36 +0900

    mysql init chnage

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ const mysql = require('mysql');
 const client = mysql.createConnection({
   host:'localhost',
   port:3306,
-  user:'hooney',
-  password:'hch',
+  user:'root',
+  password:'',
   database:'hairplanet'
 });
 
